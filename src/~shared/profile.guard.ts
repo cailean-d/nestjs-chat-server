bb0e5050dc405a2d120e51c156aca4539050ae73
Denis Lykov <cailean393@gmail.com>
2020-01-03 10:07:24 +0300

    back to express

--- src/~shared/profile.guard.ts
+++ src/~shared/profile.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { FastifyRequest } from 'fastify';
+import { Request } from 'express';
 
 @Injectable()
 export class ProfileGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request: FastifyRequest & { user: any } = context.switchToHttp().getRequest();
+    const request: Request & { user: any } = context.switchToHttp().getRequest();
     return request.user.id.toHexString() === request.params.id;
   }
 }
